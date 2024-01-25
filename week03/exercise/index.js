5a648d6cdeece5b9825915fc8717c97f4a4081f9
Tim Robillard <robillt@algonquincollege.com>
2024-01-25 18:40:39 -0500

    set up server

---
+++ week03/exercise/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (_req, res) => {
+  res.send("Server running 🚀🚀🚀");
+});
+
+const PORT = process.env.PORT ?? 4000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
