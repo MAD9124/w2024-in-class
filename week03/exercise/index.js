335e2759e083c69cb406ce11467a16579af4dffc
Tim Robillard <robillt@algonquincollege.com>
2024-01-29 18:56:11 -0500

    mon in class

--- week03/exercise/index.js
+++ week03/exercise/index.js
@@ -4,16 +4,39 @@ const express = require("express");
 const cars = require("./cars");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (_req, res) => {
   res.send("Server running 🚀🚀🚀");
 });
 
 // C - create
-app.post("/api/cars/", (req, res) => {}); // create a new car
+app.post("/api/cars/", (req, res) => {
+  const id = Date.now();
+  const { make, model, colour } = req.body;
+  if (!make || !model || !colour) {
+    res.status(400).json({
+      error: {
+        message: "Invalid input",
+      },
+    });
+    return;
+  }
+  const car = {
+    id,
+    make,
+    model,
+    colour,
+  };
+  cars.push(car);
+
+  res.status(201).json({
+    data: car,
+  });
+}); // create a new car
 
 // R - read (all)
-app.get("/api/cars/", (req, res) => {
+app.get("/api/cars/", (_req, res) => {
   res.json({
     data: cars,
   });
@@ -24,7 +47,14 @@ app.get("/api/cars/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const car = cars.find((car) => car.id === id);
 
-  // TODO error handling / send a 404 not found
+  if (!car) {
+    res.status(404).json({
+      error: {
+        message: `Car with id ${id} not found`,
+      },
+    });
+    return;
+  }
 
   res.json({
     data: car,
@@ -32,13 +62,57 @@ app.get("/api/cars/:id", (req, res) => {
 }); // return the car matching the id value
 
 // U - update (replace)
-app.patch("/api/cars/:id", (req, res) => {}); // update some properties of a car
+app.put("/api/cars/:id", (req, res) => {
+  // 1 retrieve the id
+  const id = parseInt(req.params.id);
+
+  // 2 find the matching car
+  const carIndex = cars.findIndex((car) => car.id === id);
+
+  // 3 get the new car
+  // TODO add validation
+  const newCar = {
+    id,
+    ...req.body,
+  };
+
+  // 4 replace the car in the array
+  cars[carIndex] = newCar;
+
+  res.json({
+    data: newCar,
+  });
+}); // replace all properties of a car
 
 // U - update (edit)
-app.put("/api/cars/:id", (req, res) => {}); // replace all properties of a car
+app.patch("/api/cars/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const carIndex = cars.findIndex((car) => car.id === id);
+
+  if (carIndex < 0) {
+    res.status(404).json({
+      error: {
+        message: `Car with id ${id} not found`,
+      },
+    });
+    return;
+  }
+  const newCar = {
+    ...cars[carIndex],
+    ...req.body,
+  };
+
+  cars[carIndex] = newCar;
+
+  res.json({
+    data: newCar,
+  });
+}); // update some properties of a car
 
 // D - delete (one)
-app.delete("/api/cars/:id", (req, res) => {}); // destroy the record for a car
+app.delete("/api/cars/:id", (req, res) => {
+
+}); // destroy the record for a car
 
 const PORT = process.env.PORT ?? 4000;
 app.listen(PORT, () => {
