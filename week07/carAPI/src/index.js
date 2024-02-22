bd1afc49cfa90d4e1515e6cc0cec8408aa7338e2
Tim Robillard <robillt@algonquincollege.com>
2024-02-22 18:35:02 -0500

    copy car api from week04

---
+++ week07/carAPI/src/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require("express");
+const carRouter = require('./routers/cars');
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (_req, res) => {
+  res.send("Server running 🚀🚀🚀");
+});
+
+app.use('/api/cars', carRouter);
+
+const PORT = process.env.PORT ?? 4000;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
