316f898d5039fe5300fe6832bf2c4ca6df380b0a
Tim Robillard <robillt@algonquincollege.com>
2024-03-10 13:37:02 -0400

    week09 zoom

---
+++ week09/mongodbTest/test.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/test")
+  .then(() => console.log("Connected to mongodb"))
+  .catch((e) => console.error(`Error connecting to mongodb: ${e.message}`));
+
+const Cat = mongoose.model("Cat", { name: String });
+
+const main = async () => {
+    const kitty = new Cat({ name: "Tom" });
+    const savedKitty = await kitty.save();
+
+  console.log("saved", savedKitty);
+
+  await mongoose.disconnect();
+};
+
+main();
