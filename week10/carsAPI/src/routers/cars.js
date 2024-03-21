47fb2626da49d004c37e9b8b47a115a1ebf0ed22
Tim Robillard <robillt@algonquincollege.com>
2024-03-21 19:22:58 -0400

    add isValidObjectId

--- week10/carsAPI/src/routers/cars.js
+++ week10/carsAPI/src/routers/cars.js
@@ -4,14 +4,16 @@ const { Router } = require("express");
 
 const carController = require("../controllers/cars");
 const validateCarData = require("../middlewares/validateCarData");
+const isValidObjectId = require("../middlewares/isValidObjectId");
 
 const carRouter = Router();
 
 carRouter.post("/", validateCarData, carController.create);
 carRouter.get("/", carController.getAll);
-carRouter.get("/:id", carController.getOne);
-carRouter.put("/:id", validateCarData, carController.update);
-carRouter.patch("/:id", carController.update);
-carRouter.delete("/:id", carController.deleteOne);
+
+carRouter.get("/:id", isValidObjectId, carController.getOne);
+carRouter.put("/:id", isValidObjectId, validateCarData, carController.update);
+carRouter.patch("/:id", isValidObjectId, carController.update);
+carRouter.delete("/:id", isValidObjectId, carController.deleteOne);
 
 module.exports = carRouter;
