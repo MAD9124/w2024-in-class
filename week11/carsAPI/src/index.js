4d53b51deb9b49ae63462cdd44fcbe1114d56d2a
Tim Robillard <robillt@algonquincollege.com>
2024-03-25 19:15:51 -0400

    add express-mongo-sanitize

--- week11/carsAPI/src/index.js
+++ week11/carsAPI/src/index.js
@@ -3,18 +3,21 @@
 require("dotenv/config");
 const express = require("express");
 const morgan = require("morgan");
+const expressMongoSanitize = require("express-mongo-sanitize");
 require("./models/db");
 
 const debug = require("debug")("week11");
 
 const carRouter = require("./routers/cars");
 const { errorHandler } = require("./middlewares/errors");
-const sanitizeBody = require('./middlewares/sanitizeBody');
+const sanitizeBody = require("./middlewares/sanitizeBody");
 
 const app = express();
 
 app.use(morgan("tiny"));
 app.use(express.json());
+
+app.use(expressMongoSanitize());
 app.use(sanitizeBody);
 
 app.get("/", (_req, res) => {
