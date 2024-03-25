f549aafe21339ccfb47d028b2fa4bf1e8cf9f293
Tim Robillard <robillt@algonquincollege.com>
2024-03-25 19:10:13 -0400

    updated car model

--- week11/carsAPI/src/models/cars.js
+++ week11/carsAPI/src/models/cars.js
@@ -5,6 +5,11 @@ const carSchema = new mongoose.Schema(
     make: { type: String, minLength: 3, maxLength: 64 },
     model: { type: String, minLength: 3, maxLength: 64 },
     colour: { type: String, minLength: 3, maxLength: 64 },
+    seats: [String],
+    details: {
+      battery: Number,
+      kilometres: Number,
+    },
   },
   {
     timestamps: true,
