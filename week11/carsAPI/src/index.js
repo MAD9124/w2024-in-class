1e3d88e36d14105e81db2341a1d109cd59dce8e9
Tim Robillard <robillt@algonquincollege.com>
2024-03-25 19:28:24 -0400

    add more stuff

--- week11/carsAPI/src/index.js
+++ week11/carsAPI/src/index.js
@@ -4,6 +4,9 @@ require("dotenv/config");
 const express = require("express");
 const morgan = require("morgan");
 const expressMongoSanitize = require("express-mongo-sanitize");
+const compression = require("compression");
+const cors = require("cors");
+const helmet = require("helmet");
 require("./models/db");
 
 const debug = require("debug")("week11");
@@ -19,6 +22,18 @@ app.use(express.json());
 
 app.use(expressMongoSanitize());
 app.use(sanitizeBody);
+
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production" ? process.env.CORS_WHITELIST : "*",
+  })
+);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(helmet());
+  app.use(compression());
+}
 
 app.get("/", (_req, res) => {
   res.send("Server running 🚀🚀🚀");
